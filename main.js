cb65902088d8c364396c04fc7982404d704c7962
serifcanozdeniz <ozdenizserifcan96@gmail.com>
2023-12-05 21:36:03 +0300

    html, scss, javascript kodlamaları yapıldı. Projeye devam edildi.

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-import {ele} from "./scripts/ui.js";
+import {mailData} from "./scripts/constant.js";
+import {ele, renderMails} from "./scripts/ui.js";
 
-//Navbar için açılma ve kapanma özelliği
-//Hamburger iconuna tıklanma olayını izle
-//tıklanınca nav menüsüne hide class ı ekle
-//zaten kapalıyken tıklanırsa hide class ını kaldır
+// 1) Navbar için açılma ve kapanma özelliği
+// Hamburger iconuna tıklanma olayını izle
+// tıklanınca nav menüsüne hide class ı ekle
+// zaten kapalıyken tıklanırsa hide class ını kaldır
 ele.menu.addEventListener("click", () => {
     ele.nav.classList.toggle("hide");
+});
+
+
+// 2) Listeleme özelliği
+// kullanıcı projeye girme anında mailleri listele
+// DOMContentLoaded > tarayıcıdaki html'in yüklenmesinin bitmesi
+document.addEventListener('DOMContentLoaded', () => {
+    renderMails(mailData);
 });
--- scripts/ui.js
+++ scripts/ui.js
@@ -3,4 +3,37 @@
 export const ele = {
     menu: document.querySelector("#menu"),
     nav: document.querySelector("nav"),
+    mailsArea: document.querySelector(".mails"),
+};
+
+//! ekrana mailleri basa
+// mailData: ekrana basılacak mailler
+export const renderMails = (mailData) => {
+    // mailData dizisindeki her bir mail için bir mail
+    // mail htmi'i oluştur ve mail html dizisine aktar
+ const mail_html = mailData.map((mail) => {
+    
+    return `
+        <div class="mail">
+                    <div class="info">
+                        <input type="checkbox">
+                        <i class="bi bi-star-fill"></i>
+                        <b>${mail.sender}</b>
+                    </div>
+                    <div class="content">
+                        <p class="title">${mail.title}</p>
+                        <p class="desc">${mail.message}</p>
+                    </div>
+                    <p class="time">${mail.date}</p>
+                    <button>Sil</button>
+        </div>
+        
+        `;
+    });
+
+    // join: dizi olarak tanımlanmış olan html değişkenini stringe çevirdi   
+    //  console.log(mail_html.join(' '));
+
+    // html deki mail alanına oluşturduğumuz stringi gönder
+    ele.mailsArea.innerHTML = mail_html.join(' ');
 };
